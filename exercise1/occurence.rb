59142d79e30bfe9974fd0dcfe59f84b0af6e369b
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-08 11:49:09 +0530

    Solution to Basic Ruby Occurence - Hash Exercise

---
+++ exercise1/occurence.rb
@@ -0,0 +1,7 @@
+def count_alphabet_occurence(input_string)
+  alphabet_hash = Hash.new(0)
+  input_string = input_string.downcase
+  input_string.each_char {|letter| alphabet_hash[letter] = alphabet_hash[letter] + 1 if letter >= 'a' && letter <= 'z'}
+  alphabet_hash.each {|key,value| printf("%s : %3.0f\n", key, value) }
+end
+count_alphabet_occurence(gets)
