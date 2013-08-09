5fc63a2ad8e0fa22d3a830c76170e43dd2ab08ec
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-09 19:36:09 +0530

    Separated files into two

--- exercise5/Accounts.rb
+++ exercise5/Accounts.rb
@@ -17,14 +17,3 @@ class Accounts
       puts "Sorry you cannot withdraw ", amount, ". Your account balance is insufficient."
     end
 end
-
-#Add two accounts and deposit and withdraw money
-acc1 = Accounts.new("Jatin");
-acc1.deposit(1500.50);
-acc2 = Accounts.new("ABCD");
-acc2.deposit(152.0);
-acc1.withdraw(130.00);
-#Show Account Info for each account
-puts acc1.inspect
-puts acc2.inspect
-puts acc3.inspect
