7ad41a9103bb42ecd4f41e6dd15b6a12b17f91cc
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-13 13:44:37 +0530

    Changed user experience, changed method name

--- exercise4/bin/palindrome.rb
+++ exercise4/bin/palindrome.rb
@@ -1,11 +1,14 @@
-def is_palindrome(input_string)
+#!/usr/bin/env ruby
+def palindrome?(input_string)
   while !(input_string =~ /^q$/i)
     if input_string == input_string.reverse
       print input_string + " is a palindrome\n"
     else
       print input_string + " is not a palindrome\n"
     end
+    puts "Enter a string (Enter 'q' OR 'Q' to quit):"
     input_string = gets.chomp
   end
 end
-is_palindrome(gets.chomp)
+puts "Enter a string (Enter 'q' OR 'Q' to quit):"
+palindrome?(gets.chomp)
