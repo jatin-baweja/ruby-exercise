7b7c1c18fa3e0153139c236672eb013e1542e4de
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-13 17:52:36 +0530

    Solution to Basic Ruby Power - Array Exercise

---
+++ exercise8/bin/main.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+require_relative "../lib/array"
+print [1, 2, 3, 4, 5, 6].power(3), "\n"
---
+++ exercise8/lib/array.rb
@@ -0,0 +1,5 @@
+class Array
+  def power(exponent)
+    collect {|x| x**exponent if x.is_a? Integer }
+  end
+end
