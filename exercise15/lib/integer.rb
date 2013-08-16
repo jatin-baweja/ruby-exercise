d1581d289eb5caba3624fb1f7822f27b842cfb15
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-16 15:50:38 +0530

    Used prime?

--- exercise15/lib/integer.rb
+++ exercise15/lib/integer.rb
@@ -1,12 +1,11 @@
+require "prime"
 class Integer
   def prime_numbers_upto
     case
     when self > 1
-      prime_numbers = (2..self).to_a
-      prime_numbers.each do |num|
-        (num*2).step(self, num) do |not_prime|
-          prime_numbers.delete(not_prime)
-        end
+      prime_numbers = []
+      2.step(self, 1) do |num|
+        prime_numbers.push(num) if Prime.prime?(num)
       end
       "Prime Numbers in range : " + prime_numbers.to_s
     when self == 0 || self == 1
