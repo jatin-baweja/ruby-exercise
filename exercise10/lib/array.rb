dbb45321a778dd6fcc30b8bff566ac38d4ee1322
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-16 15:32:33 +0530

    Used to_s directly, key assignment simplified

--- exercise10/lib/array.rb
+++ exercise10/lib/array.rb
@@ -1,27 +1,20 @@
 class Array
   def convert_to_hash
-    length_hash = Hash.new
+    length_hash = Hash.new {|hash, key| hash[key] = [] }
     for i in 0...length
-      if self[i].is_a? Integer
-        len = self[i].to_s.length
-      else
-        len = self[i].length
-      end
-      length_hash[len] ||= []
+      len = self[i].to_s.length
       length_hash[len].push(self[i])
     end
     length_hash
   end
+  def odd_or_even(num)
+    num.odd? ? "odd" : "even"
+  end
   def group_into_hash
     group_hash = Hash.new
-    group_hash = convert_to_hash.sort.inject({}) do |new_hash, (key,value)|
-      if key.odd?
-        new_hash["odd"] ||= []
-        new_hash["odd"].push(value)
-      else
-        new_hash["even"] ||= []
-        new_hash["even"].push(value)
-      end
+    hash_of_arrays = Hash.new {|hash, key| hash[key] = [] }
+    group_hash = convert_to_hash.sort.inject(hash_of_arrays) do |new_hash, (key,value)|
+      new_hash[odd_or_even(key)].push(value)
       new_hash
     end
     group_hash
