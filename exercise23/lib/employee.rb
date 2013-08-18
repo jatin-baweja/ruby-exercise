a12e8b4eb24e53814801b5f97c084c66205c3f4a
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-19 01:40:11 +0530

    Removed read_csv method from Employee class

--- exercise23/lib/employee.rb
+++ exercise23/lib/employee.rb
@@ -1,20 +1,8 @@
-require_relative "file"
 class Employee
   attr_reader :designation, :name, :empid
   def initialize(name, empid, designation)
     @name = name
     @empid = empid
     @designation = designation
-  end
-  def self.read_csv(file_name)
-    employee_data = []
-    File.open(File.get_full_path(file_name)) do |file|
-      while line = file.gets
-        line.chomp!
-        name, empid, designation = line.split(/\s*,\s*/)
-        employee_data.push(Employee.new(name, empid, designation))
-      end
-    end
-    employee_data
   end
 end
