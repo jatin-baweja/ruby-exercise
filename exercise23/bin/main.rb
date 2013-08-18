6c0b0e3209b58171e3fc99bb5af1ea1d775af232
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-19 01:45:01 +0530

    Calling function read_csv instead of Employee class method

--- exercise23/bin/main.rb
+++ exercise23/bin/main.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 require_relative "../lib/array"
-employee_data = Employee.read_csv("employees.csv")
+employee_data = read_csv("employees.csv")
 employee_data.store_data_in_file("employee_data.txt")
