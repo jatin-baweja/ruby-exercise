6c948aa11ba6bf7cbe60edbbd3e061b109c413c7
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-19 00:32:25 +0530

    Changed use of for-in loop

--- exercise9/lib/array.rb
+++ exercise9/lib/array.rb
@@ -1,9 +1,9 @@
 class Array
   def convert_to_hash
     hash_of_element_length = Hash.new {|hash, key| hash[key] = [] }
-    for i in 0...length
-      len = self[i].to_s.length
-      hash_of_element_length[len].push(self[i])
+    for element in self
+      len = element.to_s.length
+      hash_of_element_length[len].push(element)
     end
     hash_of_element_length
   end
