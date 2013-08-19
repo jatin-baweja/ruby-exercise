27b1bff7ed35714640fb8f183144098958d2eaa9
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-19 15:25:59 +0530

    Changed block to receive amount instead of interests

--- exercise19/bin/main.rb
+++ exercise19/bin/main.rb
@@ -4,8 +4,7 @@ puts "Enter Principal Amount: "
 principal = Integer(gets.chomp)
 puts "Enter Time(in years): "
 time = Integer(gets.chomp)
-interest = Interest.new(principal, time) do |si, ci|
-  diff = ci - si
-  diff.round(2)
+interest = Interest.new(principal, time) do |amount|
+  amount.round(2)
 end
 puts interest.difference
