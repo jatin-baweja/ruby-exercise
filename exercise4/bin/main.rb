9c48d459ad8f083811c0b8bb679be9c64cd05ebd
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-21 11:12:16 +0530

    Added UX part to main

--- exercise4/bin/main.rb
+++ exercise4/bin/main.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
-require_relative "../lib/palindrome"
+require_relative "../lib/string"
 puts "Enter a string (Enter 'q' OR 'Q' to quit):"
 input_string = gets.chomp
-check_palindrome(input_string)
+while input_string != "q" && input_string != "Q"
+  puts "#{ input_string } is #{ input_string.palindrome? ? "" : "not " }a palindrome"
+  puts "Enter a string (Enter 'q' OR 'Q' to quit):"
+  input_string = gets.chomp
+end
