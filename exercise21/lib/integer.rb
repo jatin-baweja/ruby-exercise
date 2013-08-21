2823166d7a1969cdcf51fcbeda5690135066031b
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-21 16:35:45 +0530

    Changed raise for negative numbers, changed factorial computing method

--- exercise21/lib/integer.rb
+++ exercise21/lib/integer.rb
@@ -1,16 +1,19 @@
+require_relative "exception"
 class Integer
   def factorial
-    begin
-      case
-      when self > 0
-        "Factorial is " + (1..self).inject(:*).to_s
-      when self == 0
-        "Factorial is 1"
-      else
-        raise "Negative integers not allowed"
+    case
+    when self > 0
+      factorial = 1
+      num = 1
+      while num <= self
+        factorial *= num
+        num += 1
       end
-    rescue RuntimeError => negative_value
-      puts "Error in input: #{negative_value}"
+      "Factorial is " + factorial.to_s
+    when self == 0
+      "Factorial is 1"
+    else
+      raise NegativeIntegerException, "Negative integers not allowed"
     end
   end
 end
