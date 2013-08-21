7100ac792e232a896e73207086136e32ab4b95fa
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-21 16:36:42 +0530

    Added rescue for invalid inputs

--- exercise21/bin/main.rb
+++ exercise21/bin/main.rb
@@ -2,4 +2,14 @@
 require_relative "../lib/integer"
 puts "Enter a number:"
 input_number = gets.chomp
-puts Integer(input_number).factorial
+begin
+  if input_number =~ /^[+|-]?\d+$/
+    puts input_number.to_i.factorial
+  else
+    raise NotAnIntegerException, "Input is not an integer"
+  end
+rescue NegativeIntegerException
+  puts $!
+rescue NotAnIntegerException
+  puts $!
+end
