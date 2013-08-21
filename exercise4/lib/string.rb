b5a04d621a8884930a3b16380e64523969b5a05d
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-21 11:09:54 +0530

    Changed method check_palindrome to palindrome?

--- exercise4/lib/string.rb
+++ exercise4/lib/string.rb
@@ -1,11 +1,5 @@
-def check_palindrome(input_string)
-  while !(input_string =~ /^q$/i)
-    if input_string == input_string.reverse
-      print input_string + " is a palindrome\n"
-    else
-      print input_string + " is not a palindrome\n"
-    end
-    puts "Enter a string (Enter 'q' OR 'Q' to quit):"
-    input_string = gets.chomp
+class String
+  def palindrome?
+    self == reverse
   end
 end
