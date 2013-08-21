96a5c79d9ca9b5069496fbf7b0b912ecf21cba8e
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-21 17:09:58 +0530

    Passed array to method store_data_in_file instead of using an Array method

--- exercise23/lib/data_storage.rb
+++ exercise23/lib/data_storage.rb
@@ -1,15 +1,13 @@
-require_relative "read_csv"
-class Array
-  def store_data_in_file(file_name)
-    File.open(File.get_full_path(file_name), "w") do |file|
-      group_by {|emp| emp.designation }.each do |key,value|
-        plural_key = key + "s"
-        file.puts plural_key
-        value.each do |employee|
-          file.puts "#{employee.name} (EmpId: #{employee.empid})"
-        end
-        file.puts
+require_relative "csv_reader"
+def store_data_in_file(from_array, file_name)
+  File.open(File.get_full_path(file_name), "w") do |file|
+    from_array.group_by {|emp| emp.designation }.each do |key,value|
+      plural_key = key + "s"
+      file.puts plural_key
+      value.each do |employee|
+        file.puts "#{employee.name} (EmpId: #{employee.empid})"
       end
-    end    
-  end
+      file.puts
+    end
+  end    
 end
