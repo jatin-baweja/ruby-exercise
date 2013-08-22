0d4f12fc80b41d842df1c20964d8d500b88b5115
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-22 12:10:47 +0530

    Added modularity to code

--- exercise26/lib/dictionary.rb
+++ exercise26/lib/dictionary.rb
@@ -1,15 +1,17 @@
 require_relative "string"
+def get_max_lcs_length_from_suggestions(wrong_word, suggestions, length_of_lcs_hash)
+  max_key = 0
+  suggestions.each do |suggestion|
+    key = wrong_word.length_of_longest_common_subsequence(suggestion)
+    max_key = [key, max_key].max
+    length_of_lcs_hash[key].push(suggestion)
+  end
+  max_key
+end
 class Dictionary
   def self.suggest_correct_word(wrong_word, suggestions)
-    length_of_words_hash = Hash.new { |hash, key| hash[key] = [] }
-    max_key = 0
-    suggestions.each do |suggestion|
-      key = wrong_word.length_of_longest_common_subsequence(suggestion)
-      if key > max_key
-        max_key = key
-      end
-      length_of_words_hash[key].push(suggestion)
-    end
-    length_of_words_hash[max_key]
+    length_of_lcs_hash = Hash.new { |hash, key| hash[key] = [] }
+    max_key = get_max_lcs_length_from_suggestions(wrong_word, suggestions, length_of_lcs_hash)
+    length_of_lcs_hash[max_key]
   end
 end
