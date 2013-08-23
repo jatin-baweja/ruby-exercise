3065285ba44a97202bc1dc1d4d6262bcf5188d21
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-23 11:30:57 +0530

    Used regex for comparison of quitting case

--- exercise4/bin/main.rb
+++ exercise4/bin/main.rb
@@ -2,7 +2,7 @@
 require_relative "../lib/string"
 puts "Enter a string (Enter 'q' OR 'Q' to quit):"
 input_string = gets.chomp
-while input_string != "q" && input_string != "Q"
+while input_string !~ /^q$/i
   puts "#{ input_string } is #{ input_string.palindrome? ? "" : "not " }a palindrome"
   puts "Enter a string (Enter 'q' OR 'Q' to quit):"
   input_string = gets.chomp
