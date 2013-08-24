19cc41147c810e11da6c0383efa1fad28f4e9afa
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-24 16:01:34 +0530

    Reorganised based on library changes

--- exercise17/bin/main.rb
+++ exercise17/bin/main.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
-require_relative "../lib/string"
-puts "Enter time1:"
-time1 = gets.chomp
-puts "Enter time2:"
-time2 = gets.chomp
-puts time1.sum_time(time2)
+require_relative "../lib/formatted_time"
+puts "Enter time1 (in h:m:s format) :"
+time1 = FormattedTime.new(gets.chomp)
+puts "Enter time2 (in h:m:s format) :"
+time2 = FormattedTime.new(gets.chomp)
+puts "Total Time: ", time1.add_time(time2)
