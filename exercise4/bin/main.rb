86d41d9ff67cccf3f58de534f9b0a2b2b784d74a
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-26 12:39:46 +0530

    Changed while to loop, added break when q/Q is entered

--- exercise4/bin/main.rb
+++ exercise4/bin/main.rb
@@ -4,8 +4,8 @@ def get_user_input
   puts "Enter a string (Enter 'q' OR 'Q' to quit):"
   gets.chomp
 end
-input_string = get_user_input
-while input_string !~ /^q$/i
-  puts "#{ input_string } is #{ input_string.palindrome? ? "" : "not " }a palindrome"
+loop do
   input_string = get_user_input
+  break if input_string =~ /^q$/i
+  puts "#{ input_string } is #{ input_string.palindrome? ? "" : "not " }a palindrome"
 end
