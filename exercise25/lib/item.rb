74bd38169837e5bc00bc274e6928988fe42eef8d
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-26 13:30:32 +0530

    Added Sales tax and import duty constants, defined price_incld_tax

--- exercise25/lib/item.rb
+++ exercise25/lib/item.rb
@@ -1,31 +1,41 @@
 class Item
+  SALES_TAX_RATE = 0.1
+  IMPORT_DUTY_RATE = 0.05
+  attr_reader :name, :price, :sales_tax, :import_duty, :price_incld_tax
+
   def initialize(name, price, imported, sales_tax_exempted)
     @name = name
     @price = price
     @imported = imported
     @sales_tax_exempted = sales_tax_exempted
   end
+
   def sales_tax
     if !@sales_tax_exempted
-      sales_tax = @price * 0.1
+      sales_tax = price * SALES_TAX_RATE
       sales_tax.round(2)
     else
-      0.00
+      0.0
     end    
   end
+
   def import_duty
     if @imported
-      import_duty = @price * 0.05
+      import_duty = price * IMPORT_DUTY_RATE
       import_duty.round(2)
     else
-      0.00
+      0.0
     end
   end
-  def total
-    total = @price + sales_tax + import_duty
-    total.round
+
+  def price_incld_tax
+    total = price + sales_tax + import_duty
+    total.round(2)
   end
-  def to_s
-    "#{ @name }".ljust(25) + ":" + "#{ total }".rjust(7)
+
+  def details
+    details_array = []
+    details_array.push(name, price, sales_tax, import_duty, price_incld_tax)
   end
+
 end
