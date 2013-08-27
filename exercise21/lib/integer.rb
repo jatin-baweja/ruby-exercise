cf11a57486c036b13ab974cd0ae879ad38679ea9
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-27 10:32:30 +0530

    Restructured code for factorial calculation

--- exercise21/lib/integer.rb
+++ exercise21/lib/integer.rb
@@ -2,16 +2,9 @@ require_relative "exception"
 class Integer
   def factorial
     case
-    when self > 0
+    when self >= 0
       factorial = 1
-      num = 1
-      while num <= self
-        factorial *= num
-        num += 1
-      end
-      "Factorial is " + factorial.to_s
-    when self == 0
-      "Factorial is 1"
+      factorial = self == 0 ? 1 : self * (self - 1).factorial
     else
       raise NegativeIntegerException, "Negative integers not allowed"
     end
