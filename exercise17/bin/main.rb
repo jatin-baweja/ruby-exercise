16f275362bd700c00926b32e1feae71cdf2c1537
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-27 15:32:10 +0530

    Added time and formatted received array

--- exercise17/bin/main.rb
+++ exercise17/bin/main.rb
@@ -4,4 +4,6 @@ puts "Enter time1 (in h:m:s format) :"
 time1 = FormattedTime.new(gets.chomp)
 puts "Enter time2 (in h:m:s format) :"
 time2 = FormattedTime.new(gets.chomp)
-puts "Total Time: ", time1.add_time(time2)
+unformatted_time = time1.add(time2)
+formatted_time = (unformatted_time[0] == 1 ? "1 day & " : "") + unformatted_time[1].to_s.rjust(2,'0') + ":" + unformatted_time[2].to_s.rjust(2,'0') + ":" + unformatted_time[3].to_s.rjust(2,'0')
+puts "Total Time: ", formatted_time
