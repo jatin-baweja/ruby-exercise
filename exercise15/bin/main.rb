2c92dea10eb763eafecd3aa2bce44ef28243dea8
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-27 11:37:21 +0530

    Changed from Integer() to to_i method

--- exercise15/bin/main.rb
+++ exercise15/bin/main.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 require_relative "../lib/integer"
 puts "Enter a number:"
-input_number = gets.chomp
-puts Integer(input_number).prime_numbers_upto
+input_number = gets.chomp.to_i
+puts input_number.prime_numbers_upto
