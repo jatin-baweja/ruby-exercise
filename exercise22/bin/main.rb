441ba4c3e53970bc9db6f0fab96c4c8e8a4001c0
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-27 12:36:38 +0530

    Added test case for first name nil

--- exercise22/bin/main.rb
+++ exercise22/bin/main.rb
@@ -5,5 +5,8 @@ last_name = "Xyz"
 #Call with first letter of firstname small
 name1 = Name.new(first_name, last_name)
 last_name = nil
-#Call with null value passed
+#Call with last name nil value passed
 name2 = Name.new(first_name, last_name)
+first_name = nil
+#Call with first name and last name nil
+name3 = Name.new(first_name, last_name)
