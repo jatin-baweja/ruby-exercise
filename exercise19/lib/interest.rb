9e5e6cb4227840389de84e44d18da11ed33db037
jatin-baweja <jatin.baweja@vinsol.com>
2013-09-02 16:16:56 +0530

    Initialized using yield, defined methods for rate, ci and si

--- exercise19/lib/interest.rb
+++ exercise19/lib/interest.rb
@@ -1,14 +1,25 @@
 class Interest
-  def initialize(principal, time, rate_of_interest = 10, &block)
-    @principal = principal
-    @time = time
-    @rate_of_interest = rate_of_interest
-    @block = block
+  attr_reader :principal, :time
+
+  def initialize
+    @principal, @time, @rate_of_interest = yield
   end
+
+  def rate
+    @rate_of_interest/100.0
+  end
+
+  def simple_interest
+    principal * time * rate
+  end
+
+  def compound_interest
+    principal * (1 + rate) ** time - principal
+  end
+
   def difference
-    simple_interest = @principal * @time * @rate_of_interest/100.0
-    compound_interest = @principal * (1 + @rate_of_interest/100.0)**@time - @principal
-    difference = compound_interest - simple_interest
-    "Difference in interests : #{@block.call(difference)}"
+    difference = (compound_interest - simple_interest).round(2)
+    "Difference in interests : #{ difference }"
   end
+
 end
