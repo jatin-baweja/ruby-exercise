9978bd17d08176e2d39e4c80d9e62c1fa87e5d9a
jatin-baweja <jatin.baweja@vinsol.com>
2013-10-21 13:04:21 +0530

    Used interpolation in string

--- exercise16/bin/main.rb
+++ exercise16/bin/main.rb
@@ -4,4 +4,6 @@ puts "Enter a string:"
 input_string = gets.chomp
 puts "Enter a word or letter to be searched:"
 search_word = gets.chomp
-puts input_string.find_occurence_of(search_word)
+replaced_string_and_count = input_string.find_occurence_of(search_word)
+puts "#{ replaced_string_and_count[:replacement] }"
+puts "Total Occurences found: #{ replaced_string_and_count[:count] }"
