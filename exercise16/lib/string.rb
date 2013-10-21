fb79cac7943069e86d9041856e7509899d9710fa
jatin-baweja <jatin.baweja@vinsol.com>
2013-10-21 13:03:23 +0530

    Changed regex to remove capturing group, escaped special characters and returning hash instead of string

--- exercise16/lib/string.rb
+++ exercise16/lib/string.rb
@@ -1,11 +1,11 @@
 class String
   def find_occurence_of(word)
     counter = 0
-    regex_of_word = Regexp.new("(" + word + ")","i")
+    regex_of_word = Regexp.new(Regexp.escape(word),"i")
     replaced_string = gsub(regex_of_word) do |matched_string|
       counter += 1
       "(" + matched_string + ")"
     end
-    replaced_string + "\nTotal Occurences found: " + counter.to_s
+    { :replacement => replaced_string, :count => counter }
   end
 end
